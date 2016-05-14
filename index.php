ebf125a1f8a1d61b3d661e58356870aa895c2818
serpencob <serpencob@users.noreply.github.com>
2016-05-14 13:00:22 +1200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+echo "hello"
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>My WebApp</title>
+</head>
+
+<body>
+<h1>Hello, world!</h1>
+</body>
+</html>
